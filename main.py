851bb6637188978fd0ab4a38a045658bd3e299e5
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-10 20:51:48 -0500

    se instalo flask, y se creo el  html,css y main

---
+++ main.py
@@ -0,0 +1,16 @@
+#para ejecutar
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+#Desde aqui la logica de la pagina de recetas
