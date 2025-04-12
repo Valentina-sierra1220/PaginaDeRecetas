4c159ff699b02f327ff22c3e8dd6f61a1e3a60fc
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-11 19:14:36 -0500

    se creo otro archivo html para que cuando se le de al boton entrar me mande a la pagina de inicio

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    return render_template('pagina.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
@@ -14,3 +19,4 @@ if __name__ == '__main__':
 
 
 #Desde aqui la logica de la pagina de recetas
+
